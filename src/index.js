b31a62424057124125655beeb9be4f877891907b
vidhishweta01 <vidhishweta01@gmail.com>
2021-07-03 21:23:16 +0530

    added event listener

--- src/index.js
+++ src/index.js
@@ -1,4 +1,49 @@
 import setup from './js/dom';
 import './style.css';
+import Project from './js/project';
 
+const array = [];
 setup();
+const form = document.querySelector('.form');
+
+
+function display() {
+  const contain = document.querySelector('.content');
+  contain.innerHTML = '';
+  array.forEach((project) => {
+    const Div = document.createElement('div');
+    Div.setAttribute('class', 'project');
+    const title = document.createElement('h3');
+    title.setAttribute('class', 'title');
+    title.innerHTML = project.name;
+
+    const date = document.createElement('h3');
+    date.setAttribute('class', 'title');
+    date.innerHTML = project.date;
+
+    Div.append(title, date);
+    contain.appendChild(Div);
+  });
+}
+
+function newProject(name, date) {
+  const project = new Project();
+  project.name = name;
+  project.date = date;
+  array.push(project);
+}
+
+function getData() {
+  
+
+
+}
+const submitButton = document.querySelector('.submit');
+submitButton.addEventListener('click', (getData) => {
+  getData.preventDefault();
+  const title = document.getElementById('101').value;
+  const date1 = document.getElementById('1od').value;
+  newProject(title, date1);
+  form.reset();
+  display();
+});
--- src/js/dom.js
+++ src/js/dom.js
@@ -17,6 +17,7 @@ function sideNav() {
   project.append(projHead);
 
   const addProject = document.createElement('button');
+  addProject.setAttribute('class', 'add-project');
   addProject.innerHTML = '+ Add Project';
 
   const inbx = document.createElement('button');
@@ -36,6 +37,29 @@ function sideNav() {
 function contain() {
   const element = document.createElement('div');
   element.setAttribute('class', 'content');
+  const addproject = document.createElement('button');
+  addproject.setAttribute('class', 'add-project');
+  addproject.innerHTML = '+ Add Project';
+  document.body.append(element);
+}
+
+function contain2() {
+  const element = document.createElement('form');
+  const br = document.createElement('br');
+  const br1 = document.createElement('br');
+  element.setAttribute('id', 'form');
+  element.setAttribute('class', 'form');
+  var i = document.createElement("input");
+  i.setAttribute('type',"text");
+  i.setAttribute('name', "name");
+  i.setAttribute('id', "101");
+  const gfg = document.createElement("input") 
+  gfg.setAttribute("type", "date");
+  gfg.setAttribute("id", "1od");
+  const sub = document.createElement('button');
+  sub.setAttribute('class', 'submit');
+  sub.innerHTML = 'Submit';
+  element.append(i, br1, gfg, br, sub);
   document.body.append(element);
 }
 
@@ -43,4 +67,5 @@ export default function setup() {
   TopNav();
   sideNav();
   contain();
+  contain2();
 }
