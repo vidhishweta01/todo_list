039a9b1a25bb960fbdeb1e593be98a28467723c7
vidhishweta01 <vidhishweta01@gmail.com>
2021-07-21 19:24:12 +0530

    fixed linter errors

---
+++ src/js/Test/construc.test.js
@@ -0,0 +1,20 @@
+import { newProject, newTask } from '../construc';
+
+describe('creating a new project with project modal', () => {
+  test('create new project passing in project name', () => {
+    // eslint-disable-next-line no-unused-expressions
+    expect(newProject('Shaurya')).toBeTruthy();
+  });
+});
+
+describe('Create to do and append to local storage', () => {
+  test('new todo saved to browser', () => {
+    const newTestTodo = newTask(
+      'title',
+      'description',
+      'dueDate',
+      'Shaurya',
+    );
+    expect(newTestTodo).toBeTruthy();
+  });
+});
